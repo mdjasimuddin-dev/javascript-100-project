25ba67cc69b7b054469f985ef520f4cf7647d3d4
MD JASIM UDDIN <mjunachole97@gmail.com>
2025-07-07 01:13:29 +0600

    problem solved

---
+++ 04. Digital Watch/index.js
@@ -0,0 +1,54 @@
+
+
+
+function getDay() {
+
+    let d = new Date()
+    const months = [
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+
+    const days = [
+        "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
+    ];
+
+    let day = days[d.getDay()]
+    let date = d.getDate()
+    let month = months[d.getMonth()]
+    let year = d.getFullYear()
+
+    console.log(day);
+    console.log(month);
+    console.log(year);
+
+
+    return `${day} , ${date} , ${month} , ${year}`
+
+}
+
+
+document.querySelector('.flex').innerHTML = getDay()
+
+
+
+function getTime() {
+    let d = new Date()
+    let h = d.getHours()
+    let m = d.getMinutes()
+    let s = d.getSeconds()
+
+
+    let ampm = h > 12 ? 'PM' : 'AM';
+    h = h % 12 || 12;
+    h = h < 10 ? '0'+h : h;
+    m = m < 10 ? '0'+m : m;
+    s = s < 10 ? '0'+s : s;
+
+
+    const time = `${h} : ${m} : ${s} ${ampm}`
+
+    document.querySelector('.time').innerHTML = time
+}
+
+setInterval(getTime, 1000)
